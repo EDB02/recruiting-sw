d2c7518bccc331b35f970700b45c132caa85e62e
Enrico Dal Bianco <edalbianco02@gmail.com>
2022-10-12 13:38:04 +0200

    hex conversion

--- telemetry_2/main.cpp
+++ telemetry_2/main.cpp
@@ -26,34 +26,9 @@ static FILE *log, *stats;
 static bool opened = 0;
 unordered_map<uint16_t, stat > stats_map;
 
-int split(char *message, int size)
+void parse(char *message, uint16_t &id, uint64_t &payload)
 {
-    for(int i=0;i<size;i++)
-    {
-        if(message[i] == '#') return i;
-    }
-    return -1;
-}
-
-uint64_t hex_to_int(char *message, int i1, int i2)
-{
-    uint64_t ret = 0;
-    for(int i=i1;i<i2;i++)
-    {
-        if(message[i] >= 'A' && message[i] <= 'F') ret |= (message[i]-'A'+10)<<(4*(i2-i-1));
-        else ret |= (message[i]-'0')<<(4*(i2-i-1));
-    }
-    return ret;
-}
-
-int parse(char *message, int size, uint16_t &id, uint64_t &payload)
-{
-    int index = split(message, size);
-    if(index < 0)
-        return -1;
-    id = hex_to_int(message, 0, index);
-    payload = hex_to_int(message, index+1, size);
-    return 0;
+    sscanf(message, "%X#%X", &id, &payload);
 }
 
 
@@ -73,83 +48,68 @@ int main(void){
     {
         switch(currentState)
         {
-            case IDLE:
-                bytes = can_receive(message);
-                if(parse(message, bytes, id, payload))
+        case IDLE:
+            bytes = can_receive(message);
+            parse(message, id, payload);
+            if(id == 0x0A0 && (payload == 0x6601 || payload == 0xFF01))
+            {
+                currentState = RUN;
+            }
+        break;
+        case RUN:
+            cur_time = time(0);
+            cur_time_ms = clock()/(CLOCKS_PER_SEC/1000.0);
+
+            if(!opened)
+            {
+                log = fopen((logs_path + to_string(cur_time) + ".log").c_str(), "w");
+                stats = fopen((stats_path + to_string(cur_time) + ".csv").c_str(), "w");
+                if(!log || !stats)
                 {
-                    cout<<"input error\n";
+                    cout<<"Impossibile creare i file. Forse mancano le cartelle stats e logs\n";
                     return -1;
                 }
-                if(id == 0x0A0 && (payload == 0x6601 || payload == 0xFF01))
+                opened = 1;
+            }
+
+            bytes = can_receive(message);
+
+            parse(message, id, payload);
+
+            if(id == 0x0A0 && (payload == 0x6601 || payload == 0xFF01)) //ignore start messages
+            {
+                continue;
+            }
+
+            if(id == 0x0A0 && payload == 0x66FF)    //stop message
+            {
+                for(auto i : stats_map)
                 {
-                    currentState = RUN;
+                    fprintf(stats, "%03X;%d;%.2f\n", i.first, i.second.number_of_messages, i.second.mean_time);
                 }
-                break;
-            case RUN:
-                cur_time = time(0);
-                cur_time_ms = clock()/(CLOCKS_PER_SEC/1000.0);
+                fclose(log);
+                fclose(stats);
+                opened = 0;
+                stats_map.clear();
+                currentState = IDLE;
+                continue;
+            }
 
-                if(!opened)
-                {
-                    log = fopen((logs_path + to_string(cur_time & 0xFFFFFFF) + ".log").c_str(), "w");
-                    stats = fopen((stats_path + to_string(cur_time & 0xFFFFFFF) + ".csv").c_str(), "w");
-                    if(!log || !stats)
-                    {
-                        cout<<"Impossibile creare i file. Forse mancano le cartelle stats e logs\n";
-                        return -1;
-                    }
-                    opened = 1;
-                }
+            fprintf(log, "(%lld) %s\n", cur_time, message);
 
-                bytes = can_receive(message);
+            if(!stats_map.count(id))
+            {
+                stats_map[id] = {1, 0, cur_time_ms};
+            }
+            else
+            {
+                stats_map[id].mean_time = ((stats_map[id].mean_time * stats_map[id].number_of_messages) + (cur_time_ms - stats_map[id].last_time)) / 
+                                            (stats_map[id].number_of_messages+1.0);
+                stats_map[id].last_time = cur_time_ms;
+                stats_map[id].number_of_messages++;
+            }
 
-                if(parse(message, bytes, id, payload))
-                {
-                    cout<<"input error\n";
-                    return -1;
-                }
-
-                if(id == 0x0A0 && (payload == 0x6601 || payload == 0xFF01)) //ignore start messages
-                {
-                    continue;
-                }
-
-                if(id == 0x0A0 && payload == 0x66FF)    //stop message
-                {
-                    for(auto i : stats_map)
-                    {
-                        for(int j=0;j<3;j++)
-                        {
-                            uint16_t c = ((i.first >> (4*(2-j))) & 0xF);
-                            if(c >= 10)
-                                fprintf(stats, "%c", c-10+'A');
-                            else
-                                fprintf(stats, "%c", c+'0');
-                        }
-                        fprintf(stats, ";%d;%.2f\n", i.second.number_of_messages, i.second.mean_time);
-                    }
-                    fclose(log);
-                    fclose(stats);
-                    opened = 0;
-                    stats_map.clear();
-                    currentState = IDLE;
-                    continue;
-                }
-
-                fprintf(log, "(%lld) %s\n", cur_time, message);
-
-                if(!stats_map.count(id))
-                {
-                    stats_map[id] = {1, 0, cur_time_ms};
-                }
-                else
-                {
-                    stats_map[id].mean_time = ((stats_map[id].mean_time * stats_map[id].number_of_messages) + (cur_time_ms - stats_map[id].last_time)) / (stats_map[id].number_of_messages+1.0);
-                    stats_map[id].last_time = cur_time_ms;
-                    stats_map[id].number_of_messages++;
-                }
-
-                break;
+        break;
         }
     }
 
